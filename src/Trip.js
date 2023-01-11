c4097f3f26272e2ae4e7d7ddb204389d2ea0bd44
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-01-11 16:36:35 -0500

    Implement and pass first two tests of Trip

--- src/Trip.js
+++ src/Trip.js
@@ -1,7 +1,11 @@
 const dayjs = require('dayjs');
 
 class Trip {
-
+  constructor(tripInfo) {
+    this.id = tripInfo.id;
+    this.userID = tripInfo.userID;
+    this.destinationID = tripInfo.destinationID;
+  }
 }
 
 export default Trip;
