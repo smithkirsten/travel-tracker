15475bd035d0d25f79ea9583a5ca2104baadfe6a
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-01-10 22:38:23 -0500

    Add trips test info to traveler test

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -4,7 +4,7 @@ const expect = chai.expect;
 
 import Traveler from '../src/Traveler';
 import travelersTestData from './traveler-test-data';
-
+import tripTestData from './trips-test-data';
 
 
 
@@ -20,17 +20,34 @@ describe('Traveler', () => {
   });
 
   it('should have an id and name', () => {
-    expect(traveler.id).to.equal(1)
+    expect(traveler.id).to.equal(1);
     expect(traveler.name).to.equal('Ham Leadbeater');
   });
   
   it('should have a travelType property and a trips property that defaults to an empty array', () => {
     expect(traveler.travelType).to.equal('relaxer');
     expect(traveler.trips).to.deep.equal([]);
+
+    traveler.trips = tripTestData.filter(trip => trip.userID === traveler.id);
+    expect(traveler.trips.length).to.equal(2)
+    expect(traveler.trips[0]).to.deep.equal({
+      id: 5,
+      userID: 1,
+      destinationID: 29,
+      travelers: 3,
+      date: "2022/04/30",
+      duration: 18,
+      status: "approved",
+      suggestedActivities: [ ]
+    })
   });
 
-  it('should calculate total cost of all trips'), {
-    expect()
-  }
+  it('should calculate total cost of all trips', () => {
+    expect(traveler.calcTotalCosts()).to.equal();
+  });
+
+  it('should be able to filter trips by status', () => {
+    expect(traveler.tripsByStatus()).to.equal();
+  });
 
 });
