7ee2e573389638e5f89bf4007e5df5b2f33b650a
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-01-11 21:03:07 -0500

    Add get and post functions to apiCalls and event listener to scripts

--- src/scripts.js
+++ src/scripts.js
@@ -5,9 +5,10 @@ const dayjs = require('dayjs');
 //import classes
 import Traveler from '../src/Traveler';
 import Trip from '..src/Traveler';
-import Destination from '..src/Destination'
-import DestRepo from '..src/DestRepo'
-import Agent from '..src/Agent'
+import Destination from '../src/Destination'
+import DestRepo from '../src/DestRepo'
+import Agent from '../src/Agent'
+import apiCalls from '../src/apiCalls'
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
 import './images/sunset.png'
@@ -19,7 +20,9 @@ let destRepo;
 let newTripEst;
 
 //event listeners
-window.addEventListener('load', )
+window.addEventListener('load', seeData())
+
+function seeData() 
 
 
 
