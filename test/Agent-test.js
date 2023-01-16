51f3273c34d80cfecce72b7ea41893f5390f7ef3
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-01-16 12:57:15 -0500

    Add beforeEach to agent test

--- test/Agent-test.js
+++ test/Agent-test.js
@@ -17,18 +17,21 @@ describe('Agent', () => {
   let destRepo;
   let agent;
   beforeEach(()=> {
-    travelers = travelersTestData;
-    trips = tripTestData;
+    trips = tripTestData.map(trip => new Trip(trip));
+    travelers = travelersTestData.map(traveler => {
+      const userTrips = trips.filter(trip => trip.userID === traveler.id);
+      return new Traveler (traveler, userTrips);
+    })
     destRepo = new DestRepo(destTestData);
-    agent = new Agent(travelers, trips);
+    agent = new Agent(travelers, destRepo);
     //will need to filter trips in promise resolve and assign to travelers before passing into agent?
   })
 
   it('should be an instance of Agent', () => {});
 
-  it('should hold an array of trip instances', () => {});
-
   it('should hold an array of traveler instances', () => {});
+
+  it('should hold the destRepo object as a property')
 
   it('should calculate total income generated for this year', () => {
     //Total income generated this year (should be 10% of user trip cost)
@@ -42,18 +45,17 @@ describe('Agent', () => {
   it('should be able to search user by name', () => {
     //return an object to populate a traveler card displayed to agent
     //View their name, a list of all of their trips, and the total amount they’ve spent (including 10% agent cut)
-  })
+  });
 
   it('should be able to locate a user with a trip id', () => {});
 
   it('should be able to approve and deny pending trips', () => {
     //I should be able to see and approve / deny trip requests
       //dynamic method that takes in a boolean (approve if true, deny if false)
-  })
+  });
 
   it('should be able to cancel a trip for a user', () => {
     //Delete an upcoming trip for that user
     //deletes from data model
-  })
-
-})
+  });
+});
