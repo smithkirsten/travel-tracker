1072cba9675229c3ed0fa1e258248a4d0bff374a
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-01-16 11:57:55 -0500

    Add imports to agent class

--- test/Agent-test.js
+++ test/Agent-test.js
@@ -1,3 +1,22 @@
 const dayjs = require('dayjs');
 const chai = require("chai");
 const expect = chai.expect;
+
+import travelersTestData from './traveler-test-data';
+import tripTestData from './trips-test-data';
+import destTestData from './destination-test-data';
+import Trip from '../src/Trip';
+import Traveler from '../src/Traveler'
+import DestRepo from '../src/DestRepo'
+
+
+describe('Agent', () => {
+  let travelers;
+  let trips;
+  let destRepo;
+  let agent;
+  beforeEach(()=> {
+  })
+
+  it('', () => {})
+})
