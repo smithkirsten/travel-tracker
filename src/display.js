fb33cc8be841bbdd855c31e491437f3d0be44dd4
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-01-15 22:03:09 -0500

    Employ loginError display

--- src/display.js
+++ src/display.js
@@ -5,6 +5,8 @@ import Trip from '../src/Trip';
 
 //query selectors
 const loginDisplay = document.getElementById('loginPage');
+const loginError = document.getElementById('loginError');
+
 const navBar = document.querySelector('nav');
 const accountDisplay = document.querySelector('main');
 const logoutPanel = document.getElementById('logoutPanel');
@@ -36,6 +38,7 @@ const postResponse = document.getElementById('postBox');
 const postMessage = document.getElementById('postMessage');
 
 function login(boolean) {
+  loginError.classList.add('hidden');
   if(boolean) {
     loginDisplay.classList.remove('hidden');
     accountDisplay.classList.add('hidden');
@@ -209,7 +212,7 @@ function postDeclaration(boolean) {
 }
 
 function loginError() {
-  //username and password do not match
+  loginError.classList.remove('hidden');
   console.log('login error');
 }
 
